db41bfb242bec9e97b7674421a66fe6fa8215a8d
Abshir <abshir.y.ahmed@gmail.com>
2023-03-28 13:33:51 +0100

    pair programming

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,65 @@
-import { AnswerCard } from "@/components/answerCard";
-import { useEffect, useState } from "react";
+import {AnswerCard} from "@/components/answerCard";
+import {useEffect, useState} from "react";
 import styled from "styled-components"
 import getRandomAyahs, {AyahData} from "@/src/api";
 import AudioPlayer from 'react-audio-player';
 
-const Wrapper = styled.div `
-display: flex;
-flex-direction: column;
-align-items: center;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
-const Heading = styled.div `
-margin: 20px;
-font-size: 50px;
-color: #FFF;
-`
-
-const AyahWrapper = styled.div `
-display: flex;
+const Heading = styled.div`
+  margin: 20px;
+  font-size: 50px;
+  color: #FFF;
 `
 
-const AudioWrapper = styled.div `
-margin: 20px 0px
+const AyahWrapper = styled.div`
+  display: flex;
 `
 
-const Home = () => { 
-  const [ayahs, setAyahs] = useState<AyahData[]>([]);
+const AudioWrapper = styled.div`
+  margin: 20px 0px
+`
 
-  useEffect(() => {
-    async function fetchData() {
-      const ayahData = await getRandomAyahs();
-      setAyahs(ayahData);
-    }
-    fetchData();
-  }, []);
+const Home = () => {
+    const [ayahs, setAyahs] = useState<AyahData[]>([]);
+    const [selectedAyah, setSelectedAyah] = useState<AyahData>();
 
-
-
-  return (
-   <Wrapper>
-    <Heading>Hafiz 1.0</Heading>
-    <p>Guess the surah the ayah is from</p>
-    <AyahWrapper>
-    {ayahs.map((a) => {
-      return (
-        <AnswerCard isCorrect={a.audioUrl ? true : false} key={a.number} arabicSurahName={a.arabicName} englishSurahName={a.englishName}/>
-      )
-    })}
-    </AyahWrapper>
-      <AudioWrapper>
-      {ayahs.map((a) => {
-        if(a.audioUrl){
-          return (
-            <AudioPlayer key={a.number} autoPlay={true} src={a.audioUrl} controls={true}/>
-          )
+    useEffect(() => {
+        async function fetchData() {
+            const ayahData = await getRandomAyahs();
+            setAyahs(ayahData);
         }
-          })}
-      </AudioWrapper>
-    
-   
-   </Wrapper>
-  )
+
+
+        fetchData().then(() => {
+            setSelectedAyah(ayahs[Math.floor(Math.random() * ayahs.length)]);
+            console.log({selectedAyah, ayahs})
+        });
+
+    }, []);
+
+
+    return (
+        <Wrapper>
+            <Heading>Hafiz 1.0</Heading>
+            <p>Guess the surah the ayah is from</p>
+            <AyahWrapper>
+                {selectedAyah && ayahs.map(({number,arabicName,englishName}) => {
+                    return (
+                        <AnswerCard isCorrect={englishName === selectedAyah.englishName} key={number} arabicSurahName={arabicName} englishSurahName={englishName}/>
+                    )
+                })}
+            </AyahWrapper>
+            <AudioWrapper>
+                { selectedAyah && <AudioPlayer key={selectedAyah.number} autoPlay={false} src={selectedAyah.audioUrl} controls={true}/> }
+            </AudioWrapper>
+
+
+        </Wrapper>
+    )
 }
 
 export default Home;
--- src/api.ts
+++ src/api.ts
@@ -1,56 +1,68 @@
 import axios from 'axios';
 
 export interface AyahData {
-  arabicName: string;
-  englishName: string;
-  number: number;
-  audioUrl?: string;
+    arabicName: string;
+    englishName: string;
+    number: number;
+    audioUrl?: string;
 }
 
-function generateRandomNumbers(): number[] {
+const TOTAL_AYAH_COUNT = 6236;
+const MIN_AYAH_NUMBER = 5673;
+
+
+function generateRandomAyah(numberOfSurahs = 4): number[] {
     const randomNumbers: number[] = [];
-  
-    while (randomNumbers.length < 4) {
-      const randomNumber = Math.floor(Math.random() * (6236 - 5673 + 1) + 5673);
-      if (!randomNumbers.includes(randomNumber)) {
-        randomNumbers.push(randomNumber);
-      }
+
+    for (let i = 0; i < numberOfSurahs; i++) {
+        const randomNumber = Math.floor(Math.random() * (TOTAL_AYAH_COUNT - MIN_AYAH_NUMBER + 1) + MIN_AYAH_NUMBER);
+        if (!randomNumbers.includes(randomNumber)) {
+            randomNumbers.push(randomNumber);
+        }
     }
-  
     return randomNumbers;
-  }
+}
 
 
 const getRandomAyahs = async (): Promise<AyahData[]> => {
-    const randomNumbers = generateRandomNumbers();
-    const surahData: AyahData[] = [];
-  
-    await Promise.all(
-      randomNumbers.map((number) =>
-        axios.get(`http://api.alquran.cloud/v1/ayah/${number}`).then((response) => {
-          const surah = response.data.data.surah;
-          const surahDatum: AyahData = {
+    const randomNumbers = generateRandomAyah();
+
+    const promises = randomNumbers.map((number) => axios.get(`http://api.alquran.cloud/v1/ayah/${number}/ar.alafasy`));
+    const responses = await Promise.all(promises);
+    return responses.map((response) => {
+        return {
             number: response.data.data.number,
-            arabicName: surah.name,
-            englishName: surah.englishName,
-          };
-          surahData.push(surahDatum);
-        })
-      )
-    );
+            arabicName: response.data.data.surah.name,
+            englishName: response.data.data.surah.englishName,
+            audioUrl: response.data.data.audio
+        }
+    });
 
-  const randomIndex = Math.floor(Math.random() * 4);
-  const ayahToGetAudioFor = surahData[randomIndex];
-  const audioResponse = await axios.get(`http://api.alquran.cloud/v1/ayah/${ayahToGetAudioFor.number}/ar.alafasy`);
-  ayahToGetAudioFor.audioUrl = audioResponse.data.data.audio
+    // await Promise.all(
+    //     randomNumbers.map((number) =>
+    //         axios.get(`http://api.alquran.cloud/v1/ayah/${number}`).then((response) => {
+    //             const surah = response.data.data.surah;
+    //             const surahDatum: AyahData = {
+    //                 number: response.data.data.number,
+    //                 arabicName: surah.name,
+    //                 englishName: surah.englishName,
+    //             };
+    //             surahData.push(surahDatum);
+    //         })
+    //     )
+    // );
+    //
+    // const randomIndex = Math.floor(Math.random() * 4);
+    // const ayahToGetAudioFor = surahData[randomIndex];
+    // const audioResponse = await axios.get(`http://api.alquran.cloud/v1/ayah/${ayahToGetAudioFor.number}/ar.alafasy`);
+    // ayahToGetAudioFor.audioUrl = audioResponse.data.data.audio
+    //
+    //
+    // return surahData;
+};
 
 
-    return surahData;
-  };
-
-
-
-  export default getRandomAyahs
+export default getRandomAyahs
 
  
 
